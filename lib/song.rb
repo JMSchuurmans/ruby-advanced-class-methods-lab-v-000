c7203b455085735f8e700843ff4f0979558ba0cf
Jeremy Schuurmans <jeremyschuurmans@gmail.com>
2018-05-28 16:15:20 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,65 @@
 class Song
+
   attr_accessor :name, :artist_name
+
   @@all = []
+
+  def initialize
+    @name = name
+    @artist_name = artist_name
+    @@all << self
+  end
+
+  def self.create
+    song = self.new
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.create
+    song.name = name
+    song
+  end
+
+  def self.find_by_name(name)
+    @@all.detect{|song| song.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    if self.find_by_name(name) || self.create_by_name(name)
+      return self.find_by_name(name)
+    else
+      self.create_by_name(name)
+    end
+  end
+
+  def self.alphabetical
+    @@all.sort_by! {|song| song.name}
+  end
+
+  def self.new_from_filename(file_data)
+    rows = self.split("\n")
+    songs = rows.collect do |row|
+      data = row.split(" - ")
+      artist_name = data[0]
+      name = data[1].split(".")
+
+      song = self.new
+      song.artist_name = artist_name
+      song.name = name
+      song
+    end
+    songs
+  end
+
+
+
 
   def self.all
     @@all
